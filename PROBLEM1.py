d4b1f4baf12e98c8c96b67d606bb8b454b2771d8
Marvin Wong <47411170+mdswong@users.noreply.github.com>
2019-11-27 20:26:52 +0800

    Update PROBLEM1.py

--- PROBLEM1.py
+++ PROBLEM1.py
@@ -12,8 +12,19 @@ GEAS = pd.DataFrame(data3, columns = ['Student', 'GEAS'])
 data4 = {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'ESAT': [93, 89, 88]}
 ESAT = pd.DataFrame(data4, columns = ['Student', 'ESAT'])
 
-merge = pd.merge(math, Electronics, how = 'right', on = 'Student')
-merge1 = pd.merge(merge, GEAS, how = 'right', on = 'Student')
-merge2 = pd.merge(merge1, ESAT, how = 'right', on = 'Student')
+merge_wide = pd.merge(math, Electronics, how = 'right', on = 'Student')
+merge_wide1 = pd.merge(merge_wide, GEAS, how = 'right', on = 'Student')
+merge_wide2 = pd.merge(merge_wide1, ESAT, how = 'right', on = 'Student')
 
-print(merge2)
+merge_long = pd.melt(merge_wide2, id_vars = 'Student', value_vars = ['Math', 'Electronics', 'GEAS', 'ESAT'])
+merge_long1 = merge_long.rename(columns = {'variable': 'Subjects', 'value': 'Grades'})
+merge_long2 = merge_long1.sort_values('Student').reset_index().drop(columns = 'index')
+
+print('\n')
+print('Wide Format:')
+print('\n')
+print(merge_wide2)
+print('\n')
+print('Long Format:')
+print('\n')
+print(merge_long2)
